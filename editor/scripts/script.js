8dd9270e6861b4953faaf14d1cb9210d096708f9
Tin Tran <tindl88@gmail.com>
2016-04-06 10:40:21 +0700

    …

--- editor/scripts/script.js
+++ editor/scripts/script.js
@@ -6,11 +6,26 @@ registerCommand();
 
 function stateProps(){
 	divEditor.addEventListener('click', function(event){
-		var isBold = document.queryCommandState ('bold');
+
+  var char = 3, sel; // character at which to place caret
+divEditor.focus();
+if (document.selection) {
+  sel = document.selection.createRange();
+  sel.moveStart('character', char);
+  sel.select();
+}
+else {
+   sel = window.getSelection();
+  sel.collapse(divEditor.firstChild, char);
+}
+
+
+
 		var isItalic = document.queryCommandState ('italic');
 		var isUnderline = document.queryCommandState ('underline');
 
-		console.log(isBold, isItalic, isUnderline);
+
+		console.log(selection);
 	});
 }
 
@@ -23,4 +38,31 @@ function registerCommand(){
 			document.execCommand(command, null, null);
 		});
 	}
+}
+function getSelected() {
+    var text = "";
+    if (window.getSelection
+    && window.getSelection().toString()
+    && $(window.getSelection()).attr('type') != "Caret") {
+        text = window.getSelection();
+        return text;
+    }
+    else if (document.getSelection
+    && document.getSelection().toString()
+    && $(document.getSelection()).attr('type') != "Caret") {
+        text = document.getSelection();
+        return text;
+    }
+    else {
+        var selection = document.selection && document.selection.createRange();
+
+        if (!(typeof selection === "undefined")
+        && selection.text
+        && selection.text.toString()) {
+            text = selection.text;
+            return text;
+        }
+    }
+
+    return false;
 }
