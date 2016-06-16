87f789d460e6c837cab43e21cdec3c7b87890ae5
Tin Tran <tindl88@gmail.com>
2016-06-17 00:59:35 +0700

    …

--- fabricjs-photo-editor/scripts/bzphoto-editor.js
+++ fabricjs-photo-editor/scripts/bzphoto-editor.js
@@ -1,194 +1,147 @@
-var bzPhotoEditor = (function(){
-	var settings = {
-		maxWidth: 767,
-		maxHeight: 1023
-	};
-	var editor = document.getElementById('bzPhotoEditor');
-	var editorImage = document.getElementById('editorImage');
-	var canvas = new fabric.Canvas('canvas', {backgroundColor: '#ccc'});
+(function(){
+	'use strict';
+	angular
+	.module('bzApp', [])
+	.controller('photoEditorCtrl', photoEditorCtrl)
+	.directive('photoEditor', photoEditor);
 
-	function resizeImage(image){
-		var ratio = 1;
+	function photoEditorCtrl($scope){
+		var pe = this;
+		pe.command = 'editor';
+		pe.active = '';
 
-		if(image.width > settings.maxWidth){
-			ratio = settings.maxWidth / image.width;
-			image.style.maxWidth = image.width * ratio;
-		}
-		else if(image.height > settings.maxHeight){
-			ratio = settings.maxHeight / image.height;
-			image.style.maxHeight = image.height * ratio;
-		}
-	}
+		pe.sendCommand = sendCommand;
 
-	function resizeCanvas(imgObject, reverse){
-		if(reverse === true){
-			canvas.setWidth(imgObject.getHeight());
-			canvas.setHeight(imgObject.getWidth());
-		} else {
-			canvas.setWidth(imgObject.getWidth());
-			canvas.setHeight(imgObject.getHeight());
-		}
-	}
+		function sendCommand(cmd){
+			if(pe.command === 'crop'){
 
-	function filter(type){
-		var pic = canvas.getItemsByUnique('mainPic')[0];
-
-		pic.filters = [];
-
-		switch(type){
-			case 'Sepia':
-			pic.filters.push(new fabric.Image.filters.Sepia());
-			break;
-			case 'Grayscale':
-			pic.filters.push(new fabric.Image.filters.Grayscale());
-			break;
-			case 'Brightness':
-			pic.filters.push(new fabric.Image.filters.Brightness({ brightness: 100 }));
-			break;
-		}
-
-		pic.applyFilters(function(){
-			apply();
-			canvas.renderAll.bind(canvas);
-		});
-	}
-
-	function rotate(type){
-		var angle = canvas.item(0).getAngle();
-
-		if(type === 'Left' || type === 'Right'){
-			if(angle === 0 || angle === 180 || angle === -180){
-				resizeCanvas(canvas.item(0), true);
 			} else {
-				resizeCanvas(canvas.item(0));
+				console.log('Send:', cmd);
 			}
 		}
-
-		switch(type){
-			case 'Left':
-			canvas.item(0).set({angle:(angle === -270 ? 0 : angle - 90), left:canvas.width / 2, top:canvas.height / 2, originX:'center', originY:'center'});
-			break;
-			case 'Right':
-			canvas.item(0).set({angle:(angle === 270 ? 0 : angle + 90), left:canvas.width / 2, top:canvas.height / 2, originX:'center', originY:'center'});
-			break;
-			case 'FlipH':
-			canvas.item(0).toggle('flipX');
-			break;
-			case 'FlipV':
-			canvas.item(0).toggle('flipY');
-			break;
-		}
-
-		apply();
-		canvas.renderAll();
-	}
-	function crop(type){
-		var resizableId = $( "#resizable");
-		var widthImg = $("#container-image").width();
-		var heightImg = $("#container-image").height();
-		if(type == 'Custom'){
-			resizableId.css({"display":"block"});
-			destroyResizable();
-			resizableId.resizable({
-		      containment: "#container-image"
-		    });
-		}
-		if(type == '1:1'){
-			resizableId.css({"display":"block"});
-			destroyResizable();
-			resizable();
-			var width = (widthImg/2);
-			resizableId.css({"width":width, "height":width});
-			resizableId.css({top: (heightImg - width)/2,left: (widthImg - width)/2});
-		}
-		if(type == '16:9'){
-			resizableId.css({"display":"block"});
-			destroyResizable();
-			resizable();
-			var width = (widthImg/2)* (16/9);
-			var height = (heightImg/2)* (16/9);
-			resizableId.css({"width":width, "height":height});
-			resizableId.css({top: (heightImg - height)/2,left: (widthImg - width)/2});
-		}
-		if(type == '4:3'){
-			resizableId.css({"display":"block"});
-			destroyResizable();
-			resizable();
-			var width = (widthImg/2)* (4/3);
-			var height = (heightImg/2)* (4/3);
-			resizableId.css({"width":width, "height":height});
-			resizableId.css({top: (heightImg - height)/2,left: (widthImg - width)/2});
-		}
 	}
 
-	function init(){
-		var editorRect = editor.getBoundingClientRect();
-		settings.maxWidth = editorRect.width;
-		settings.maxHeight = editorRect.height;
+	function photoEditor($timeout){
+		return {
+			link: function(scope, iElement, iAttrs){
+				var settings = {
+					maxWidth: 767,
+					maxHeight: 1023,
+					padding: 10
+				};
 
-		canvas.addImage({unique:'mainPic', url:'images/test.jpg'}, function(img1){
-			img1.crossOrigin = "Anonymous";
-			resizeCanvas(img1);
-			editorImage.src = img1.getSrc();
-			editorImage.onload = function(){
-				canvas.add(img1);
-				resizeImage(editorImage);
-			};
-		});
+				var currentImage = {
+					ratio: 1,
+					width: 0,
+					height: 0,
+					x: 0,
+					y: 0
+				};
+
+				var editorImage = document.getElementById('editor-image');
+				var container = iElement.find('#photo-container');
+				var resizer = iElement.find('#resizable');
+
+				init('images/test1.jpg');
+
+				scope.pe.init = init;
+				scope.pe.crop = crop;
+
+				function resizeImage(image){
+					if(image.width > settings.maxWidth){
+						currentImage.ratio = settings.maxWidth / image.width;
+						image.style.maxWidth = image.width * currentImage.ratio;
+					} else if(image.height > settings.maxHeight){
+						currentImage.ratio = settings.maxHeight / image.height;
+						image.style.maxHeight = image.height * currentImage.ratio;
+					} else {
+						currentImage.ratio = 1;
+					}
+
+					currentImage.width = image.width;
+					currentImage.height = image.height;
+					currentImage.x = image.offsetLeft;
+					currentImage.y = image.offsetTop;
+				}
+
+				function filter(type){
+
+				}
+
+				function rotate(type){
+
+				}
+
+				function crop(type){
+
+					switch(type){
+						case 'custom':
+						var size = (currentImage.width / 1) * 1;
+						resizer
+						.css({
+							left: currentImage.x + settings.padding,
+							top: currentImage.y + settings.padding,
+							width: currentImage.width - (settings.padding * 2),
+							height: currentImage.height - (settings.padding * 2)
+						})
+						.data('uiResizable')._aspectRatio = false;
+						break;
+						case '1:1':
+						var w = (currentImage.width > currentImage.height ? currentImage.height : currentImage.width);
+						var size = (w / 1) * 1;
+						resizer
+						.css({
+							left: (currentImage.x + ((currentImage.width) / 2)) - (size / 2),
+							top: (currentImage.y + ((currentImage.height) / 2)) - (size / 2),
+							width: w,
+							height: size
+						})
+						.data('uiResizable')._aspectRatio = true;
+						break;
+						case '16:9':
+						var w = (currentImage.width > currentImage.height ? currentImage.height : currentImage.width);
+						var size = (w / 16) * 9;
+						resizer
+						.css({
+							left: (currentImage.x + (currentImage.width) / 2) - (w / 2),
+							top: (currentImage.y + ((currentImage.height) / 2)) - (size / 2),
+							width: w,
+							height: size
+						})
+						.data('uiResizable')._aspectRatio = true;
+						break;
+						case '4:3':
+						var w = (currentImage.width > currentImage.height ? currentImage.height : currentImage.width);
+						var size = (w / 4) * 3;
+						resizer
+						.css({
+							left: (currentImage.x + (currentImage.width) / 2) - (w / 2),
+							top: (currentImage.y + ((currentImage.height) / 2)) - (size / 2),
+							width: w,
+							height: size
+						})
+						.data('uiResizable')._aspectRatio = true;
+						break;
+					}
+					// tính left
+					resizer.css({visibility: 'visible'});
+				}
+
+				function init(imgUrl){
+					var editorRect = container[0].getBoundingClientRect();
+					settings.maxWidth = editorRect.width;
+					settings.maxHeight = editorRect.height;
+
+					editorImage.src = imgUrl;
+					editorImage.onload = function(){
+						resizeImage(editorImage);
+
+						resizer
+						.resizable({containment: '#editor-image', aspectRatio: false})
+						.draggable({containment: '#editor-image', scroll: false});
+					};
+				}
+			}
+		};
 	}
-
-	function apply(){
-		editorImage.src = canvas.toDataURL();
-		setTimeout(resizeImage(editorImage), 200);
-	}
-
-	function resizable(){
-		var resizableId = $( "#resizable");
-		resizableId.resizable({
-	      containment: "#container-image",
-	      aspectRatio: true
-	    });
-	}
-
-	function destroyResizable(){
-		var resizableId = $( "#resizable");
-		resizableId.resizable( "destroy" );
-	}
-
-	function draggable(){
-		var resizableId = $( "#resizable");
-		resizableId.draggable({ containment: "#container-image", scroll: false });
-	}
-
-	$(document).ready(function(){
-		resizable();
-		draggable();
-	});
-	$(window).load(function(){
-		setTimeout(function(){
-			var resizableId = $( "#resizable");
-			var widthImg = $("#container-image").width();
-			var heightImg = $("#container-image").height();
-			var width = (widthImg/2);
-			resizableId.css({"width":width, "height":width});
-			resizableId.css({top: (heightImg - width)/2,left: (widthImg - width)/2});
-		});
-	});
-	return {
-		init: init,
-		filter: filter,
-		rotate: rotate,
-		crop: crop,
-		resizable: resizable,
-		draggable: draggable,
-		destroyResizable: destroyResizable
-	};
 })();
-
-$(function(){
-	bzPhotoEditor.init();
-});
-
-
-
-
