55949db92fe93722a577540c8d230eab14aeb1e6
Tin Tran <tindl88@gmail.com>
2016-06-22 18:02:33 +0700

    ...

--- fabricjs-photo-editor/scripts/bzphoto-editor.js
+++ fabricjs-photo-editor/scripts/bzphoto-editor.js
@@ -24,13 +24,13 @@
 	function photoEditor($timeout){
 		return {
 			link: function(scope, iElement, iAttrs){
-				var editorImage = document.getElementById('editor-image');
 				var container = iElement.find('#photo-container');
+				var editorImage = document.getElementById('editor-image');
+				var browseFile = iElement.find('#browseFile');
 				var resizer = iElement.find('#resizable');
-
 				var settings = {
-					maxWidth: 767,
-					maxHeight: 1023,
+					maxWidth: container.width(),
+					maxHeight: container.height(),
 					padding: 10
 				};
 
@@ -42,41 +42,160 @@
 					y: 0
 				};
 
-				var game, bg, bgRotate = 0;
+				// Phaser variables
+				var game, bg;
 
-				scope.pe.init = init;
 				scope.pe.crop = crop;
 				scope.pe.rotate = rotate;
+				scope.pe.filter = filter;
 
-				init('images/test1.jpg');
+				browseFile.on('change', uploadHandle);
 
-				function resizeImage(image){
-					if(image.width > settings.maxWidth){
-						currentImage.ratio = settings.maxWidth / image.width;
-						image.style.maxWidth = image.width * currentImage.ratio;
-					} else if(image.height > settings.maxHeight){
-						currentImage.ratio = settings.maxHeight / image.height;
-						image.style.maxHeight = image.height * currentImage.ratio;
-					} else {
-						currentImage.ratio = 1;
+				initPhaser(this);
+
+				function uploadHandle(){
+					var reader = new FileReader();
+					reader.readAsDataURL(browseFile[0].files[0]);
+					reader.onload = function (e) {
+						var imageUrl = e.target.result;
+
+						loadImage(imageUrl, function(){
+							game.scale.setGameSize(this.width, this.height);
+							game.cache.addImage('background', imageUrl, this);
+							bg.loadTexture('background', 0, 0);
+							resizeImage(this);
+							recalc(this);
+							initDraggable();
+							initResizeable();
+
+							console.log(currentImage);
+						});
 					}
+				}
 
-					currentImage.width = image.width;
-					currentImage.height = image.height;
-					currentImage.x = image.offsetLeft;
-					currentImage.y = image.offsetTop;
+				function initPhaser(){
+					var config = {
+						width: 1,
+						height: 1,
+						renderer: Phaser.AUTO,
+						parent: 'canvas',
+						resolution: 1,
+						preserveDrawingBuffer: true
+					};
+					game = new Phaser.Game(config);
+					var Play = function(game){};
+
+					Play.prototype = {
+						render: function(){
+						},
+						preload:function(){
+							game.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
+							game.load.image('background', 'images/blank.png');
+							game.load.script('gray', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/Gray.js');
+							game.load.script('sepia', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/pixi/SepiaFilter.js');
+							game.load.script('noise', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/pixi/NoiseFilter.js');
+							game.load.script('colorMatrix', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/pixi/ColorMatrixFilter.js');
+						},
+						create:function(){
+							bg = game.add.sprite(0, 0, 'background');
+							bg.anchor.setTo(0, 0);
+						},
+						update:function(){
+						}
+					};
+
+					game.state.add('Play', Play);
+					game.state.start('Play');
+				}
+
+				function resizeImage(imageInst){
+					editorImage.src = imageInst.src;
+					editorImage.onload = function(){
+						if(editorImage.width > settings.maxWidth){
+							currentImage.ratio = settings.maxWidth / editorImage.width;
+							editorImage.style.maxWidth = editorImage.width * currentImage.ratio;
+						} else if(editorImage.height > settings.maxHeight){
+							currentImage.ratio = settings.maxHeight / editorImage.height;
+							editorImage.style.maxHeight = editorImage.height * currentImage.ratio;
+						} else {
+							currentImage.ratio = 1;
+						}
+					};
+				}
+
+				function recalc(imageInst){
+					currentImage.width = imageInst.width;
+					currentImage.height = imageInst.height;
+					currentImage.x = imageInst.offsetLeft;
+					currentImage.y = imageInst.offsetTop;
+				}
+
+				function frame(type){
+
 				}
 
 				function filter(type){
+					//bg.tint = Math.random() * 0xffffff;
+					//var gray = game.add.filter('Sepia');
+					var se = new PIXI.SepiaFilter();
+					se.sepia = 0.5;
 
+					var no = new PIXI.NoiseFilter();
+					no.noise = 1;
+
+					var colorMatrix =  [
+					1,0,0,0.5,
+					0,1,0,0.5,
+					0,0,1,0.5,
+					0,0,0,1
+					];
+
+					var colorMatrix =  [
+					1,0,0,-0.5,
+					0,1,0,-0.5,
+					0,0,1,-0.5,
+					0,0,0,1
+					];
+					var co = new PIXI.ColorMatrixFilter();
+					co.matrix = colorMatrix;
+
+					bg.filters = [co];
+					getBase64();
 				}
 
-				function rotate(type){
+				function rotate(data){
+					if(data.action === 'rotate'){
+						bg.anchor.setTo(0.5, 0.5);
+						bg.angle = data.param === 'left' ? bg.angle - 90 : bg.angle + 90;
 
+						if(bg.angle === -180 || bg.angle === 0){
+							game.scale.setGameSize(currentImage.width, currentImage.height);
+							bg.x = currentImage.width / 2;
+							bg.y = currentImage.height / 2;
+
+						} else {
+							game.scale.setGameSize(currentImage.height, currentImage.width);
+							bg.x = currentImage.height / 2;
+							bg.y = currentImage.width / 2;
+						}
+					}
+
+					if(data.action === 'flip'){
+						bg.anchor.setTo(0, 0);
+						bg.x = 0;
+						bg.y = 0;
+
+						if(data.param === 'horizontal'){
+							bg.scale.x *= -1;
+						} else {
+							bg.scale.y *= -1;
+						}
+					}
+
+					getBase64();
 				}
 
 				function crop(type){
-
 					switch(type){
 						case 'custom':
 						var size = (currentImage.width / 1) * 1;
@@ -130,76 +249,27 @@
 					resizer.css({visibility: 'visible'});
 				}
 
-				function rotate(){
-					// bgRotate += 90;
-					// game.width = 125;
-					// game.height = 200;
-					// game.canvas.width = 125;
-					// game.canvas.height = 200;
-					// $('canvas').css({width:125, height:200});
-					var output = game.canvas.toDataURL('image/jpeg', 1.0);
-					//init(output);
-					$('#editor-image').attr('src', output);
+				function loadImage(imageUrl, callback){
+					var image = new Image();
+					image.src = imageUrl;
+					image.onload = callback || angular.noop();
+					return image;
 				}
 
-				function init(imgUrl){
-					var editorRect = container[0].getBoundingClientRect();
-					settings.maxWidth = editorRect.width;
-					settings.maxHeight = editorRect.height;
-
-					editorImage.src = imgUrl;
-					editorImage.onload = function(){
-						resizeImage(editorImage);
-
-						resizer
-						.resizable({containment: '#editor-image', aspectRatio: false})
-						.draggable({containment: '#editor-image', scroll: false});
-
-						initPhaser();
-					};
+				function initDraggable(){
+					resizer.draggable({containment: '#editor-image', scroll: false});
 				}
 
-				function initPhaser(imgUrl){
-					var config = {
-						width: currentImage.width,
-						height: currentImage.height,
-						renderer: Phaser.AUTO,
-						parent: 'canvas',
-						resolution: 1,
-						preserveDrawingBuffer: true
-					};
-					game = new Phaser.Game(config);
-					var Play = function(game){};
-
-					Play.prototype = {
-						render: function(){
-						},
-						preload:function(){
-							game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
-							//game.load.image('background', 'images/test1.jpg');
-						},
-						create:function(){
-							// bg = game.add.sprite(0, 0, 'background');
-							// bg.anchor.setTo(0.5, 0.5);
-							// bg.x = bg.width / 2;
-							// bg.y = bg.height / 2;
-						},
-						update:function(){
-							bg.angle = bgRotate;
-						}
-					};
-
-					game.state.add('Play', Play);
-					game.state.start('Play');
+				function initResizeable(){
+					resizer.resizable({containment: '#editor-image', aspectRatio: false})
 				}
 
-				loader = new Phaser.Loader(game);
-				loader.image('someimage','/url/to/image');
-				loader.onLoadComplete.addOnce(onLoaded);
-				loader.start();
-				var onLoaded = function(){
-					console.log('everything is loaded and ready to be used');
-				};
+				function getBase64(){
+					$timeout(function(){
+						var output = game.canvas.toDataURL('image/jpeg', 1.0);
+						$('#editor-image').attr('src', output);
+					}, 100);
+				}
 			}
 		};
 	}
